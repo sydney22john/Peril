933f397f59df98bae13fe9fa0787a77a118df897
Sydney John <sydney22john@gmail.com>
2024-05-24 22:31:08 -0700

    added a docker file to enable the rabbitMQ stomp plugin

--- rabbit.sh
+++ rabbit.sh
@@ -3,7 +3,7 @@
 case "$1" in
     start)
         echo "Starting RabbitMQ container..."
-        docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
+        docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 -p 61613:61613 rabbitmq-stomp
         ;;
     stop)
         echo "Stopping RabbitMQ container..."
